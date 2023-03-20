3523697269e67a7d68377fcf5acf71dd54de5645
SunMk1 <kmk3296@gmail.com>
2023-03-20 20:25:02 +0900

    .

--- java-lang-boot/app/src/main/java/com/bitcamp2/javaLangBoot/variable/Exam4.java
+++ java-lang-boot/app/src/main/java/com/bitcamp2/javaLangBoot/variable/Exam4.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Exam4 {
 
   @GetMapping("/test1")
-  public String test1(int v1, int v2, String op) {
+  public Object test1(int v1, int v2, String op) {
     int result = 0;
     switch (op) {
       case "+": result =v1 + v2; break;
@@ -20,24 +20,36 @@ public class Exam4 {
       case "%": result =v1 % v2; break;
       default: return "해당 연산을 수행 할 수 없습니다";
     }
-    return "=>" + result;
-
     //    이렇게 서버에서 웹브라우저가 출력할 화면을
     //    HTML을 만들어 보내는 것을
     //    "서버측 렌더링(server-side-rendering)" 이라 부른다
 
-    //    String html = "<!DOCTYPE html>"
-    //    + "<html>"
-    //    + "<head>"
-    //    + "<meta charset=\"UTF-8\">"
-    //    + "<title>변수 활용</title>"
-    //    + "</head>"
-    //    + "<body>"
-    //    + "<h1>계산 결과</h1>"
-    //    + "<p>" + v1 + " " + op + " " + v2 + " " + result + "</p>"
-    //    + "</body>"
-    //    + "</html>";
-    //    
-    //    return html;
+    String html = "<!DOCTYPE html>"
+        + "<html>"
+        + "<head>"
+        + "<meta charset=\"UTF-8\">"
+        + "<title>변수 활용</title>"
+        + "</head>"
+        + "<body>"
+        + "<h1>계산 결과</h1>"
+        + "<p>" + v1 + " " + op + " " + v2 + " " + "=" + " "+ result + "</p>"
+        + "</body>"
+        + "</html>";
+
+    return html;
+  }
+
+  @GetMapping("/test2")
+  public Object test2(int v1, int v2, String op) {
+    int result = 0;
+    switch (op) {
+      case "+": result =v1 + v2; break;
+      case "-": result =v1 - v2; break;
+      case "*": result =v1 * v2; break;
+      case "/": result =v1 / v2; break;
+      case "%": result =v1 % v2; break;
+      default: return "해당 연산을 수행 할 수 없습니다";
+    }
+    return result;
   }
 }
