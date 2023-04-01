9bc233dc3db1ae2104d5a2ca5dca519ba0da44f1
SunMk1 <kmk3296@gmail.com>
2023-04-02 01:20:31 +0900

    .

--- mylist-boot/app/src/main/java/com/bitcamp2/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/bitcamp2/mylist/ContactController.java
@@ -6,14 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ContactController {
 
+  Object[] contactList = new Object[5];
+  int count = 0;
+
   @GetMapping("/contact/list")
   public Object list() {
-    String[] arr = {"aaa", "aaa@test.com", "010-1111-2222", "aaaCompany"};
-    return arr;
+    return contactList;
   }
 
-  //  @GetMapping("/contact/add")
-  //  public Object add() {
-  //
-  //  }
+  @GetMapping("/contact/add")
+  public int add(String name, String email, String tel, String company) {
+    String[] arr = {name, email, tel, company};
+    if (count == 5) {
+      return 0;
+    }
+    contactList[count] = arr;
+    count++;
+    return 1;
+  }
 }
