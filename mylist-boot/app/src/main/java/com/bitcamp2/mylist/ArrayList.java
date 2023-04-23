4edf9837312d4e3481197cbe399d4ac2dad2f123
SunMk1 <kmk3296@gmail.com>
2023-04-23 21:35:39 +0900

    .

---
+++ mylist-boot/app/src/main/java/com/bitcamp2/mylist/ArrayList.java
@@ -0,0 +1,52 @@
+package com.bitcamp2.mylist;
+
+public class ArrayList {
+  static Contact[] contacts = new Contact[5];
+  static int size = 0;
+
+  static Contact[] toArray() {
+    Contact[] arr = new Contact[size];
+    for (int i = 0; i < size; i++) {
+      arr[i] = contacts[i];
+    }
+    return arr;
+  }
+
+  static Contact[] grow() {
+    Contact[] arr = new Contact[newlength()];
+    copy(contacts, arr);
+    return arr;
+  }
+
+  static int newlength() {
+    return contacts.length + (contacts.length >> 1);
+  }
+
+  static void copy(Contact[] source, Contact[] target) {
+    int length = source.length;
+    if (target.length < source.length) {
+      length = target.length;
+    }
+    for (int i = 0; i < length; i++) {
+      target[i] = source[i];
+    }
+  }
+
+  static int indexOf(String email) {
+    for (int i = 0; i < size; i++) {
+      if (contacts[i].email.equals(email)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  static Contact remove(int index) {
+    Contact old = contacts[index];
+    for (int i = index + 1; i < size; i++) {
+      contacts[i - 1] = contacts[i];
+    }
+    size--;
+    return old;
+  }
+}
--- mylist-boot/app/src/main/java/com/bitcamp2/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/bitcamp2/mylist/ContactController.java
@@ -6,90 +6,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ContactController {
 
-  Contact[] contactList = new Contact[5];
-  int count = 0;
-
   @GetMapping("/contact/list")
   public Object list() {
-    Contact[] arr = new Contact[count];
-    for (int i = 0; i < count; i++) {
-      arr[i] = contactList[i];
-    }
-    return arr;
+    return ArrayList.toArray(); 
   }
 
   @GetMapping("/contact/add")
   public int add(Contact contact) {
-    if(contactList.length == count) {
-      contactList = grow();
+    if(ArrayList.contacts.length == ArrayList.size) {
+      ArrayList.contacts = ArrayList.grow();
     }
-    contactList[count++] = contact;
-    return count;
+    ArrayList.contacts[ArrayList.size++] = contact;
+    return ArrayList.size;
   }
 
   @GetMapping("/contact/get")
   public Object get(String email) {
-    int index = indexOf(email);
+    int index = ArrayList.indexOf(email);
     if (index == -1) {
       return "";
     }
-    return contactList[index];
+    return ArrayList.contacts[index];
   }
 
   @GetMapping("/contact/update")
   public Object update(Contact contact) {
-    int index = indexOf(contact.email);
+    int index = ArrayList.indexOf(contact.email);
     if (index == -1) {
       return "";
     }
-    contactList[index] = contact;
-    return contactList[index];
+    ArrayList.contacts[index] = contact;
+    return ArrayList.contacts[index];
   }
 
   @GetMapping("/contact/delete")
   public Object delete(String email) {
-    int index = indexOf(email);
+    int index = ArrayList.indexOf(email);
     if (index == -1) {
       return 0;
     }
-    return remove(index);
-  }
-
-  int indexOf(String email) {
-    for (int i = 0; i < count; i++) {
-      if (contactList[i].email.equals(email)) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  Contact remove(int index) {
-    Contact old = contactList[index];
-    for (int i = index + 1; i < count; i++) {
-      contactList[i - 1] = contactList[i];
-    }
-    count--;
-    return old;
-  }
-
-  Contact[] grow() {
-    Contact[] arr = new Contact[newlength()];
-    copy(contactList, arr);
-    return arr;
-  }
-
-  int newlength() {
-    return contactList.length + (contactList.length >> 1);
-  }
-
-  void copy(Contact[] source, Contact[] target) {
-    int length = source.length;
-    if (target.length < source.length) {
-      length = target.length;
-    }
-    for (int i = 0; i < length; i++) {
-      target[i] = source[i];
-    }
+    return ArrayList.remove(index);
   }
 }
